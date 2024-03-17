3c53e58df5bb509a6b5c0c419aef8fb3a95bf515
Sathish Vanga <92833519+sathishvanga@users.noreply.github.com>
2024-03-17 18:18:26 +0530

    Update app.py

--- sentimental_analysis/app.py
+++ sentimental_analysis/app.py
@@ -1,61 +1,19 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import pickle
-import numpy as np
-app = Flask(__name__)
-import os
 from sklearn.feature_extraction.text import CountVectorizer
+import os
+from text_prep import clean  # Import the clean function from the cleaning module
 
-import string  # Add this line to import the string module
-import pandas as pd
-import emoji
-import re
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer, LancasterStemmer, SnowballStemmer, WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-import autocorrect
-import nltk
-import warnings
-
-warnings.filterwarnings('ignore')
-lemmatizer = WordNetLemmatizer()
-def clean(doc):
-    # This text contains a lot of <br/> tags.
-    doc = doc.replace("</br>", " ")
-    
-    # Remove punctuation and numbers.
-    doc = "".join([char for char in doc if char not in string.punctuation and not char.isdigit()])
-
-    # Converting to lower case
-    doc = doc.lower()
-    
-    # Tokenization
-    tokens = nltk.word_tokenize(doc)
-
-    # Lemmatize
-    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
-
-    # Stop word removal
-    stop_words = set(stopwords.words('english'))
-    filtered_tokens = [word for word in lemmatized_tokens if word.lower() not in stop_words]
-    
-    # Join and return
-    return " ".join(filtered_tokens)
-# Get the absolute path to the current script
-script_path = os.path.abspath(__file__)
+app = Flask(__name__)
 
 # Set the working directory to the script's directory
-os.chdir(os.path.dirname(script_path))
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# Specify the path to the model folder relative to the script's directory
-model_folder = "resources"
-
-# Construct the full path to the model file
-model_file_path = os.path.join(model_folder, 'sentimental_analysis.pkl')
-model_count = os.path.join(model_folder, 'count_vect.pkl')
 # Load sentimental analysis model from the pickle file
-with open(model_file_path, 'rb') as f:
+with open('resources/sentimental_analysis.pkl', 'rb') as f:
     model = pickle.load(f)
-with open(model_count, 'rb') as f:
+
+with open('resources/count_vect.pkl', 'rb') as f:
     count_vectorizer = pickle.load(f)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -63,9 +21,9 @@ def index():
     sentiment = None
     if request.method == 'POST':
         text = request.form['text']
-        cleaned_text = text.lower().strip()  # Assuming simple text cleaning
+        cleaned_text = clean(text)  # Clean the text using the clean function
         text_vectorized = count_vectorizer.transform([cleaned_text])
-        sentiment = model.predict(text_vectorized)[0]  # Get sentiment prediction
+        sentiment = model.predict(text_vectorized)[0]
     return render_template('index.html', sentiment=sentiment)
 
 if __name__ == '__main__':
