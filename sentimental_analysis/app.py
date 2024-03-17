a7ae8ff095ece8ec403ebc25c8af309bc6fc2246
Sathish Vanga <92833519+sathishvanga@users.noreply.github.com>
2024-03-17 18:35:20 +0530

    Update app.py

--- sentimental_analysis/app.py
+++ sentimental_analysis/app.py
@@ -1,19 +1,43 @@
 from flask import Flask, render_template, request
 import pickle
-from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
 import os
-from text_prep import clean  # Import the clean function from the cleaning module
+from sklearn.feature_extraction.text import CountVectorizer
+import string
+import pandas as pd
+import emoji
+import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+import nltk
+import warnings
+
+warnings.filterwarnings('ignore')
 
 app = Flask(__name__)
 
-# Set the working directory to the script's directory
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+lemmatizer = WordNetLemmatizer()
 
-# Load sentimental analysis model from the pickle file
-with open('resources/sentimental_analysis.pkl', 'rb') as f:
+def clean_text(doc):
+    doc = doc.replace("</br>", " ")
+    doc = "".join([char for char in doc if char not in string.punctuation and not char.isdigit()])
+    doc = doc.lower()
+    tokens = nltk.word_tokenize(doc)
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens = [word for word in lemmatized_tokens if word.lower() not in stop_words]
+    return " ".join(filtered_tokens)
+
+script_path = os.path.abspath(__file__)
+os.chdir(os.path.dirname(script_path))
+model_folder = "resources"
+model_file_path = os.path.join(model_folder, 'sentimental_analysis.pkl')
+model_count = os.path.join(model_folder, 'count_vect.pkl')
+
+with open(model_file_path, 'rb') as f:
     model = pickle.load(f)
-
-with open('resources/count_vect.pkl', 'rb') as f:
+with open(model_count, 'rb') as f:
     count_vectorizer = pickle.load(f)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,11 +45,10 @@ def index():
     sentiment = None
     if request.method == 'POST':
         text = request.form['text']
-        cleaned_text = clean(text)  # Clean the text using the clean function
+        cleaned_text = text.lower().strip()  
         text_vectorized = count_vectorizer.transform([cleaned_text])
-        sentiment = model.predict(text_vectorized)[0]
+        sentiment = model.predict(text_vectorized)[0]  
     return render_template('index.html', sentiment=sentiment)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
+    app.run(debug=True)
